6fc051d76fe9e34e71e52346e864eeee5fed07cb
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-08-12 22:12:06 +0900

    [level 0] Title: 모음 제거, Time: 8.19 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(my_string: String): String {
+        var answer: String = ""
+        answer = my_string.replace("a", "")
+        answer = answer.replace("e", "")
+        answer = answer.replace("i", "")
+        answer = answer.replace("o", "")
+        answer = answer.replace("u", "")
+        return answer
+    }
+}
