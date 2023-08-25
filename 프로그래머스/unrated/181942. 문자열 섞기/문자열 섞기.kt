03cad004be56352111bc1fe2d097ea3839f5a184
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-08-25 21:40:18 +0900

    [unrated] Title: 문자열 섞기, Time: 13.85 ms, Memory: 59.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(str1: String, str2: String): String {
+        var answer: String = ""
+        for(i in 0..str1.length - 1) {
+            answer = answer.plus(str1[i])
+            answer = answer.plus(str2[i])
+        }
+        return answer
+    }
+}
