fab703feee3d7a557dc4217c38ac9f600d9cca84
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-12-14 23:47:25 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.09 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
@@ -1,7 +1,13 @@
 class Solution {
     fun solution(my_string: String, overwrite_string: String, s: Int): String {
         var answer: String = ""
-        answer = my_string.substring(0 until s) + overwrite_string + my_string.substring(s + overwrite_string.length)
+        val charArray = my_string.toCharArray()
+        
+        for(i in s..s + overwrite_string.length - 1) {
+            charArray[i] = overwrite_string[i - s]
+        }
+        answer = String(charArray)
         return answer
+        
     }
 }
