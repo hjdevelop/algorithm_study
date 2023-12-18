d0d461241d5c5f5fbe41f10ded6ed51f44f4eb13
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-12-18 23:39:30 +0900

    [level unrated] Title: n의 배수, Time: 0.01 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181937. n의 배수/n의 배수.kt
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun solution(num: Int, n: Int): Int {
         var answer: Int = 0
-        if(num % n == 0) answer = 1
+        if (num % n == 0) answer = 1
         else answer = 0
         return answer
     }
