ed37083522fc30b76b6f95dc3bb658b12083460f
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-12-18 23:31:11 +0900

    [level unrated] Title: 더 크게 합치기, Time: 9.61 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.kt
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.kt
@@ -1,16 +1,13 @@
 class Solution {
     fun solution(a: Int, b: Int): Int {
         var answer: Int = 0
-        var aString: String = a.toString() + b.toString()
-        var bString: String = b.toString() + a.toString()
-        if(aString.toInt() > bString.toInt()) {
-            answer = aString.toInt()
-        }
-        else if(aString.toInt() < bString.toInt()) {
-            answer = bString.toInt()
+        val num1 = (a.toString() + b.toString()).toInt()
+        val num2 = (b.toString() + a.toString()).toInt()
+        if (num1 >= num2) {
+            answer = num1
         }
         else {
-            answer = aString.toInt()
+            answer = num2
         }
         return answer
     }
