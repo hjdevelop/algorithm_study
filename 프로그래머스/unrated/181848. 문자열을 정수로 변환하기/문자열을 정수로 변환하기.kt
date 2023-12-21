7d16143ff736e1dc4f7b3c06b2f835083ee399c2
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-12-21 19:39:58 +0900

    [level unrated] Title: 문자열을 정수로 변환하기, Time: 0.02 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.kt
+++ 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(n_str: String): Int {
-        var answer: Int = 0
-        for(i in 0..n_str.length - 1) {
-            answer *= 10
-            answer += n_str[i].digitToInt()
-        }
-        return answer
-    }
+    fun solution(n_str: String): Int = n_str.toInt()
 }
