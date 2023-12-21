e88a54ef2009c273b3154d5b201d91cb21a9cdf5
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-12-21 22:55:43 +0900

    [level unrated] Title: 문자열안에 문자열, Time: 6.94 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(str1: String, str2: String): Int {
+        var answer = 0
+        if (str1.contains(str2)) answer = 1
+        else answer = 2
+        
+        return answer
+    }
+}
