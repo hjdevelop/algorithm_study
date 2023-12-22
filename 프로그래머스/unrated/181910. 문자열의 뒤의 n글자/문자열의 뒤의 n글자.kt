31ec3bf9e33ef7122d19a8c4038d82dbb4fc0234
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-12-22 20:19:27 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, Time: 0.01 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.kt
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.kt
@@ -1,7 +1,3 @@
 class Solution {
-    fun solution(my_string: String, n: Int): String {
-        var answer: String = ""
-        answer = my_string.substring(my_string.length - n)
-        return answer
-    }
+    fun solution(my_string: String, n: Int): String = my_string.substring(my_string.length - n)
 }
