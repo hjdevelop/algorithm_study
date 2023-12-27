ff5b932c5c207ae37bbfd06b91cd01606460414d
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-12-27 20:46:07 +0900

    [level unrated] Title: n개 간격의 원소들, Time: 5.57 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.kt
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.kt
@@ -1,9 +1,3 @@
 class Solution {
-    fun solution(num_list: IntArray, n: Int): IntArray {
-        var answer: IntArray = intArrayOf()
-        for(i in 0..num_list.size - 1 step(n)) {
-            answer = answer.plus(num_list[i])
-        }
-        return answer
-    }
+    fun solution(num_list: IntArray, n: Int): IntArray = num_list.filterIndexed { index, _ -> index % n == 0 }.toIntArray()
 }
