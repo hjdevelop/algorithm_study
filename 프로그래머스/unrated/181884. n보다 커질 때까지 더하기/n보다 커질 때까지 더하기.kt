e00cb4005b535671c277e4c1fc0138cbd70adb78
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2023-12-27 21:45:27 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
@@ -1,16 +1,9 @@
 class Solution {
     fun solution(numbers: IntArray, n: Int): Int {
-        var answer: Int = 0
-        var i: Int = 0
-        while(true) {
-            answer += numbers[i]
-            if(answer > n) {
-                break
-            }
-            else {
-                i++
-                continue
-            }
+        var answer : Int = 0
+        for (i in numbers) {
+            answer += i
+            if (answer > n) break
         }
         return answer
     }
