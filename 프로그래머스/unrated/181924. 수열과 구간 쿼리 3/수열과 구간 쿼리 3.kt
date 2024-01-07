6d16e81a21fa875813bbaa85dd76fcca71e47f14
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2024-01-07 23:35:22 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 60.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
+        var answer = arr
+        
+        for (i in queries) {
+            val temp = answer[i[0]]
+            answer[i[0]] = answer[i[1]]
+            answer[i[1]] = temp
+        }
+        return answer
+    }
+}
