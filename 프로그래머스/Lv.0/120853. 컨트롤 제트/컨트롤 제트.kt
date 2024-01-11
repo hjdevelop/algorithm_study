e74a685f484480f3b8a38f499d302b2540055bd6
hjdevelop <126463915+hjdevelop@users.noreply.github.com>
2024-01-11 23:25:28 +0900

    [level Lv.0] Title: 컨트롤 제트, Time: 7.96 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120853. 컨트롤 제트/컨트롤 제트.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(s: String): Int {
+        var answer: Int = 0
+        val list = s.split(" ")
+        
+        for (i in 0 until list.size) {
+            if (list[i].toIntOrNull() != null) answer += list[i].toInt()
+            else answer -= list[i - 1].toInt()
+        }
+        return answer
+    }
+}
